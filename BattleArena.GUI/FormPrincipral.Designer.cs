8b59d1ba3ca8095ab71f5e681d355bae1c0d9bda
Gabriel Balbinot <gabrielfdr.balbinot@gmail.com>
2022-12-05 15:36:01 -0300

    Início do projeto - primeiros arquivos

---
+++ BattleArena.GUI/FormPrincipral.Designer.cs
@@ -0,0 +1,50 @@
+﻿namespace BattleArena.GUI
+{
+    partial class FormPrincipral
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FormPrincipral
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FormPrincipral";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.FormPrincipral_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormPrincipral_Paint);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ BattleArena.GUI/FormPrincipral.cs
@@ -0,0 +1,61 @@
+﻿using BattleArena.GameLogic;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace BattleArena.GUI
+{
+    public partial class FormPrincipral : Form
+    {
+        DispatcherTimer gameLoopTimer { get; set; }
+        Bitmap screenBuffer { get; set; }
+        Graphics screenPainter { get; set; }
+        Background background { get; set; }
+        List<GameObject> gameObjects { get; set; }
+        public FormPrincipral()
+        {
+            InitializeComponent();
+
+            this.ClientSize = Media.fundo.Size;
+            this.screenBuffer = new Bitmap(Media.fundo.Width, Media.fundo.Height);
+            this.screenPainter = Graphics.FromImage(this.screenBuffer);
+            this.gameObjects = new List<GameObject>();
+            this.background = new Background(this.screenBuffer.Size, this.screenPainter);
+
+            this.gameLoopTimer = new DispatcherTimer(DispatcherPriority.Render);
+            this.gameLoopTimer.Interval = TimeSpan.FromMilliseconds(16.6666);
+            this.gameLoopTimer.Tick += GameLoop;
+
+            this.gameObjects.Add(background);
+
+            StartGame();
+        }
+        public void StartGame()
+        {
+            this.gameLoopTimer.Start();
+        }
+
+        public void GameLoop(object sender, EventArgs e)
+        {
+            foreach(GameObject gObj in this.gameObjects) 
+            {
+                gObj.UpdateObject();
+                this.Invalidate();
+            }
+        }
+
+        private void FormPrincipral_Load(object sender, EventArgs e)
+        {           
+
+        }
+
+        private void FormPrincipral_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+            e.Graphics.DrawImage(this.screenBuffer, 0, 0);
+        }
+    }
+}
